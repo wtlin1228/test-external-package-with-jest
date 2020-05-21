11d35ccfd99aef83f9562ff37f9ec486b3abcbd8
wtlin1228 <wtlin1228@gmail.com>
2020-05-21 17:59:06 +0800

    before eject

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import KA from 'KA'
+import './App.css'
 
 function App() {
+  const { name, birthday } = KA.getUserProfile()
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>name: {name}</h3>
+      <h3>birthday: {birthday}</h3>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
